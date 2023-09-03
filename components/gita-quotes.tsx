de37fbd2dad5b51251b079d08f61f2e07c1c1c3e
Deveesh Shetty <deveeshshetty@gmail.com>
2023-09-03 17:50:19 +0530

    Adds UI Styles and background image

--- components/gita-quotes.tsx
+++ components/gita-quotes.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { chapter_verse } from "../lib/gita"
 import { getRandomNumber } from "../lib/utils"
+import { GitaAPI } from "../lib/types"
 
 export default function GitaQuotes() {
   const random_number = getRandomNumber(18)
@@ -11,11 +12,11 @@ export default function GitaQuotes() {
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["gita-quote"],
+    queryKey: ["gita-quote", chapter.chapter, chapter.verse],
     refetchOnWindowFocus: false,
     retry: 5,
     queryFn: async () => {
-      return fetch(
+      const repsonse: GitaAPI = await fetch(
         `https://bhagavad-gita3.p.rapidapi.com/v2/chapters/${chapter.chapter}/verses/${chapter.verse}/`,
         {
           headers: {
@@ -24,21 +25,44 @@ export default function GitaQuotes() {
           },
         }
       ).then((res) => res.json())
+      return repsonse
     },
   })
 
   console.log(quotes)
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return (
+      <div className="flex gap-2 items-center">
+        <p>Take a breathe!</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-loader-2 animate-spin text-2xl"
+        >
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+      </div>
+    )
   }
 
   return (
-    <div>
-      <p>
-        Chapter {quotes.chapter_number} - Verse {quotes.verse_number}
-      </p>
+    <div className="text-center mt-8 flex flex-col gap-4">
+      <p className="text-2xl">{quotes.text}</p>
       <p>{quotes.translations[0].description}</p>
+      {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi
+        vitae ullam unde consequatur voluptates amet, reiciendis tenetur
+        laboriosam quisquam?
+      </p> */}
     </div>
   )
 }
